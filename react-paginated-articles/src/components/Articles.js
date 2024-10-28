b391040a042d03d5578397aae204594ab21de240
Chid <chidambararaja97@gmail.com>
2024-10-28 12:01:26 +0530

    deleted

--- react-paginated-articles/src/components/Articles.js
+++ react-paginated-articles/src/components/Articles.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Articles = () => {
-
   const [totalPages, setTotalPages] = useState(0);
   const [articles, setArticles] = useState([]);
-  const link = 'https://jsonmock.hackerrank.com/api/articles?page=';
+  const link = "https://jsonmock.hackerrank.com/api/articles?page=";
 
   const apiCall = async (page) => {
     let url = link + page;
@@ -15,9 +14,9 @@ const Articles = () => {
 
     setTotalPages(pages);
     const filteredArticles = data.data.filter((item) => item.title);
-    
+
     setArticles(filteredArticles);
-  }
+  };
 
   useEffect(() => {
     apiCall(1);
@@ -26,29 +25,37 @@ const Articles = () => {
   const handleClick = (e) => {
     let targetBtn = e.target.innerHTML;
     apiCall(targetBtn);
-  }
+  };
 
-    return (
-      
-      <React.Fragment>
-        <div className="pagination">
-          {Array(totalPages).fill().map((page, index) =>   {
+  return (
+    <React.Fragment>
+      <div className="pagination">
+        {Array(totalPages)
+          .fill()
+          .map((page, index) => {
             return (
-              <button data-testid="page-button" key={"page-button-" + index} onClick={handleClick}>{index + 1}</button>
-            )
-          })
-          }
-        </div>
-        
-        <ul className="results">
-          {articles.map((article, index) => {
-            return (
-              <li key={"title-" + index} data-testid="result-row">{article.title}</li>
-            )
+              <button
+                data-testid="page-button"
+                key={"page-button-" + index}
+                onClick={handleClick}
+              >
+                {index + 1}
+              </button>
+            );
           })}
-        </ul>
-      </React.Fragment>
-    );
-}
+      </div>
+
+      <ul className="results">
+        {articles.map((article, index) => {
+          return (
+            <li key={"title-" + index} data-testid="result-row">
+              {article.title}
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
+  );
+};
 
 export default Articles;
